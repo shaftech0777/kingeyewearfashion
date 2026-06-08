6fa54daa16f4b7dc89e87928f09eec7a65a9f207
Shaf Tech <shaftech0777@gmail.com>
2026-06-08 23:34:18 +0500

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/kingeyewearfashion/",
+
+  plugins: [react()],
+
   tanstackStart: {
-    server: { entry: "server" }
+    server: {
+      entry: "server"
+    }
   }
 });
